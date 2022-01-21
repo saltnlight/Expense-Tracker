fdea040fa565627b1378cdf7fa0337ba7578ccc5
saltnlight <arongflorence@yahoo.com>
2022-01-21 12:15:55 +0100

    add props to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    // const eDate = new Date(2021, 2, 28);
+    // const eTitle = 'Food';
+    // const eAmount = 10.40;
+
     return (
         <div className='expense-item'>
-            <div>March 10th, 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h3>Food</h3>
-                <div className='expense-item__price'>$10.00</div>
+                <h3>{props.title}</h3>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     )
