e8dfd8957ee25fabcfb27d5d6a52a827858bb2a6
saltnlight <arongflorence@yahoo.com>
2022-01-21 16:47:42 +0100

    split ExpenseItem comp. into ExpenseItemDate. cleaned App.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,10 @@
 import './ExpenseItem.css'
+import ExpenseItemDate from './ExpenseItemDate';
 
 function ExpenseItem(props) {
-    // const eDate = new Date(2021, 2, 28);
-    // const eTitle = 'Food';
-    // const eAmount = 10.40;
-
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <ExpenseItemDate date={props.date}/>
             <div className='expense-item__description'>
                 <h3>{props.title}</h3>
                 <div className='expense-item__price'>${props.amount}</div>
